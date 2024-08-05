822e5be15aa755ba936e7dc0c3ebd09082d448f0
venila <venilagunda2003@gmail.com>
2024-08-05 17:33:48 +0530

    king

---
+++ ArithemeticOperators.java
@@ -0,0 +1,26 @@
+
+ class ArithemeticOperators
+{
+public static void main(String [] args)
+{
+   int a=10;
+   int b=20;
+   int c=a+b;
+   System.out.println("Addition of two numbers are " +c);
+   
+   int x=20;
+   int y=10;
+   int z=x-y;
+   System.out.println("Subtraction of two numbers are " +z);
+
+   int p=12;
+   int q=6;
+   int r=p*q;
+   System.out.println("Multiplication of two numbers are " +r);
+
+   int d1=12;
+   int d2=12;
+   int d3=d1/d2;
+   System.out.println("Division of two numbers are " +d3);
+}
+}
